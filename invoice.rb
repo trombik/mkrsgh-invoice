ec7474635af7a2fbb5598272d6b3101e75a20a62
Tomoyuki Sakurai <y@trombik.org>
2022-04-20 11:46:05 +0700

    bugfix: rubocop'fy

--- invoice.rb
+++ invoice.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 require "erb"
 require "yaml"
 
-config = YAML.load(File.read(ARGV[0]))
+config = YAML.safe_load(File.read(ARGV[0]))
 input = File.read("template.erb")
 output = ERB.new(input).result_with_hash(config)
 puts output
