5f7001d0759317ff15dcccf4f2e0b895ef5b1bc4
Tomoyuki Sakurai <y@trombik.org>
2022-04-20 16:25:17 +0700

    feat: split config.yml into issuer.yml and invoice.yml

--- invoice.rb
+++ invoice.rb
@@ -4,7 +4,10 @@
 require "erb"
 require "yaml"
 
-config = YAML.safe_load(File.read(ARGV[0]))
+invoice_config = YAML.safe_load(File.read(ARGV[0]))
+config = YAML.safe_load(File.read("issuer.yml"))
+config.merge!(invoice_config)
+
 input = File.read("template.erb")
 output = ERB.new(input).result_with_hash(config)
 puts output
